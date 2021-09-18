433c2816d4713cdff80b8fd4c3a15203f1afaf55
Naresh Karthi <90091205+narkxze@users.noreply.github.com>
2021-09-18 19:59:56 +0530

    Delete StringReplace.java

--- week3.day1/Strings/StringReplace.java
+++
@@ -1,9 +0,0 @@
-package org.strings;
-
-public class StringReplace {
-	public static void main(String[] args) {
-		String text = "Java Exercise";
-		System.out.println("The Index of E is: "+text.indexOf('E'));
-		System.out.println("The Index of s is: "+text.indexOf('s'));
-	}
-}
