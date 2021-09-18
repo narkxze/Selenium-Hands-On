4922f5ee16ec101077c5b70b61928dda087074d2
நரேஷ் கார்த்தி <90091205+narkxze@users.noreply.github.com>
2021-09-18 20:07:26 +0530

    Add files via upload

---
+++ week3-day1/Computer-Desktop/Desktop.java
@@ -0,0 +1,7 @@
+package org.system;
+
+public class Desktop extends Computer {
+	public  void desktopSize(double displaySize) {
+		System.out.println("The size of the display is "+displaySize+" inches");
+	}	
+}
